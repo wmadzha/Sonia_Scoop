19d2de04d44c7b18e8964c6fe1b4cd7909c4e999
Wan Mohd Adzha <wmadzha11@hotmail.com>
2018-12-28 23:27:13 +0800

    Update BoardMountingAssemblyController.cs

--- Controllers/BoardMountingAssemblyController.cs
+++ Controllers/BoardMountingAssemblyController.cs
@@ -3,14 +3,14 @@ public class PCB_Mounting_AssemblyController : ApiController
     [HttpGet]    
     public List<string> GetOnlineMountingMachine (string Date)
     {
-        // Allowed For Everybody Working In Assembly Area
+        // Allowed For Everybody Working In SMT Board Mounting Assembly Area
         // Return SMT Machine Running At A Specific Date
        return null;
     }
     [HttpGet]    
     public List<string> GetMachineOutput (string Date , string SMTMachineName)
     {
-       // Allowed For Everybody Working In Assembly Area
+       // Allowed For Everybody Working In SMT Board Mounting Assembly Area
        // Return PCB Board Ouput Serial Number Of A Specific Date Of A Particular SMT Machine 
        return null;
     }
@@ -18,7 +18,7 @@ public class PCB_Mounting_AssemblyController : ApiController
     [HttpGet]    
     public List<string> GetMachineOperators (string Date , string SMTMachineName)
     {
-       // Allowed Only For Assembly Manager
+       // Allowed Only For SMT Board Mounting Assembly Manager
        // Return Individual Resonsible For The SMT Machine During Specific Date
        return null;
     }
