2dc46ac2bfa28c8fb959651719982878e6148dca
Wan Mohd Adzha <wmadzha11@hotmail.com>
2019-01-12 22:34:18 +0800

    Update Warehouse_Component_StoreController .cs

--- Controllers/Warehouse_Component_StoreController .cs
+++ Controllers/Warehouse_Component_StoreController .cs
@@ -1,6 +1,7 @@
 // Sample Warehouse API Used In Enterprise Solution
 public class Warehouse_Component_StoreController : ApiController
 {
+    [AuthorizeEnterpriseClaim("warehouse_assembly_module", "warehouse_basic")]
     [HttpGet]    
     public List<string> GetAllComponentListItems ()
     {
@@ -8,13 +9,15 @@ public class Warehouse_Component_StoreController : ApiController
         // Return All Component Or Parts That Is Used In SMT Board/Assemblies/Packaging/Logistic Factory Lines
        return null;
     }
+    [AuthorizeEnterpriseClaim("warehouse_assembly_module", "warehouse_smt")]
     [HttpGet]    
     public List<string> GetMountingBoardComponentItems ()
     {
         // Allowed For Everybody Working In SMT Board Mounting Area
         // Return All Component Or Parts That Is Used In SMT Board Mounting Assemblies
        return null;
-    }    
+    }
+    [AuthorizeEnterpriseClaim("warehouse_assembly_module", "warehouse_product")]
     [HttpGet]    
     public List<string> GetProductAssembliesComponentItems ()
     {
@@ -22,6 +25,7 @@ public class Warehouse_Component_StoreController : ApiController
        // Return All Component Or Parts That Is Used In Product Assembly Area
        return null;
     }
+    [AuthorizeEnterpriseClaim("warehouse_assembly_module", "warehouse_packaging")]
     [HttpGet]    
     public List<string> GetPackagingWarehouseComponentItems ()
     {
@@ -29,6 +33,7 @@ public class Warehouse_Component_StoreController : ApiController
        // Return All Component Or Parts That Is Used In Packaging Area
        return null;
     }
+    [AuthorizeEnterpriseClaim("warehouse_assembly_module", "warehouse_basic")]
     [HttpGet]    
     public List<string> GetLogisticComponentItems ()
     {
