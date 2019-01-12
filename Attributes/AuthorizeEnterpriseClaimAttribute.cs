81bf6534bc54277fd262505f2d8cf7ba9182dda2
Wan Mohd Adzha <wmadzha11@hotmail.com>
2019-01-12 21:01:25 +0800

    Create AuthorizeEnterpriseClaimAttribute.cs

---
+++ Attributes/AuthorizeEnterpriseClaimAttribute.cs
@@ -0,0 +1,33 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+using System.Linq;
+using System.Security.Claims;
+namespace FactoryEnterpriseSample
+{
+    public class AuthorizeEnterpriseClaimAttribute : TypeFilterAttribute
+    {
+        public AuthorizeEnterpriseClaimAttribute(string claimType, string claimValue) : base(typeof(AuthorizeEnterpriseClaimFilter))
+        {
+            Arguments = new object[] { new Claim(claimType, claimValue) };
+        }
+    }
+    public class AuthorizeEnterpriseClaimFilter : IAuthorizationFilter
+    {
+        readonly Claim _claim;
+
+        public AuthorizeEnterpriseClaimFilter(Claim claim)
+        {
+            _claim = claim;
+        }
+
+        public void OnAuthorization(AuthorizationFilterContext context)
+        {
+            var hasClaim = context.HttpContext.User.Claims.Any(c => c.Type == _claim.Type && c.Value.Split(',').Contains(_claim.Value));
+            if (!hasClaim)
+            {
+                context.Result = new ForbidResult();
+            }
+        }
+    }
+}
