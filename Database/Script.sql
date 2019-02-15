ba8a3fe9f9aa13e751224269b0eff5a1153fe6b0
Wan Mohd Adzha <wmadzha11@hotmail.com>
2019-02-15 21:59:26 +0100

    Create Script.sql

---
+++ Database/Script.sql
@@ -0,0 +1,470 @@
+/****** Object:  Table [dbo].[ApiClaims]    Script Date: 15/2/2019 9:40:31 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+--Create API Claims
+CREATE TABLE [dbo].[ApiClaims](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[Type] [nvarchar](200) NOT NULL,
+	[ApiResourceId] [int] NOT NULL,
+ CONSTRAINT [PK_ApiClaims] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+ -- Create API Properties
+ CREATE TABLE [dbo].[ApiProperties](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[Key] [nvarchar](250) NOT NULL,
+	[Value] [nvarchar](2000) NOT NULL,
+	[ApiResourceId] [int] NOT NULL,
+ CONSTRAINT [PK_ApiProperties] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+ -- Create API Resources
+ CREATE TABLE [dbo].[ApiResources](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[Enabled] [bit] NOT NULL,
+	[Name] [nvarchar](200) NOT NULL,
+	[DisplayName] [nvarchar](200) NULL,
+	[Description] [nvarchar](1000) NULL,
+	[Created] [datetime2](7) NOT NULL,
+	[Updated] [datetime2](7) NULL,
+	[LastAccessed] [datetime2](7) NULL,
+	[NonEditable] [bit] NOT NULL,
+ CONSTRAINT [PK_ApiResources] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+ -- Create API Scope Claims
+ CREATE TABLE [dbo].[ApiScopeClaims](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[Type] [nvarchar](200) NOT NULL,
+	[ApiScopeId] [int] NOT NULL,
+ CONSTRAINT [PK_ApiScopeClaims] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+ -- Create API Scope
+ CREATE TABLE [dbo].[ApiScopes](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[Name] [nvarchar](200) NOT NULL,
+	[DisplayName] [nvarchar](200) NULL,
+	[Description] [nvarchar](1000) NULL,
+	[Required] [bit] NOT NULL,
+	[Emphasize] [bit] NOT NULL,
+	[ShowInDiscoveryDocument] [bit] NOT NULL,
+	[ApiResourceId] [int] NOT NULL,
+ CONSTRAINT [PK_ApiScopes] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+ -- Create API Secret
+ CREATE TABLE [dbo].[ApiSecrets](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[Description] [nvarchar](1000) NULL,
+	[Value] [nvarchar](4000) NOT NULL,
+	[Expiration] [datetime2](7) NULL,
+	[Type] [nvarchar](250) NOT NULL,
+	[Created] [datetime2](7) NOT NULL,
+	[ApiResourceId] [int] NOT NULL,
+ CONSTRAINT [PK_ApiSecrets] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+ -- Create Client Claims
+ CREATE TABLE [dbo].[ClientClaims](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[Type] [nvarchar](250) NOT NULL,
+	[Value] [nvarchar](250) NOT NULL,
+	[ClientId] [int] NOT NULL,
+ CONSTRAINT [PK_ClientClaims] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+ -- Create Client Cors Origins
+ CREATE TABLE [dbo].[ClientCorsOrigins](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[Origin] [nvarchar](150) NOT NULL,
+	[ClientId] [int] NOT NULL,
+ CONSTRAINT [PK_ClientCorsOrigins] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+ -- Create Client Grant Type
+ CREATE TABLE [dbo].[ClientGrantTypes](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[GrantType] [nvarchar](250) NOT NULL,
+	[ClientId] [int] NOT NULL,
+ CONSTRAINT [PK_ClientGrantTypes] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+ -- Create Client IDP Restrictions
+CREATE TABLE [dbo].[ClientIdPRestrictions](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[Provider] [nvarchar](200) NOT NULL,
+	[ClientId] [int] NOT NULL,
+ CONSTRAINT [PK_ClientIdPRestrictions] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+ -- Create Client Post Logout Redirect UriS
+CREATE TABLE [dbo].[ClientPostLogoutRedirectUris](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[PostLogoutRedirectUri] [nvarchar](2000) NOT NULL,
+	[ClientId] [int] NOT NULL,
+ CONSTRAINT [PK_ClientPostLogoutRedirectUris] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+ -- Create Client Properties
+CREATE TABLE [dbo].[ClientProperties](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[Key] [nvarchar](250) NOT NULL,
+	[Value] [nvarchar](2000) NOT NULL,
+	[ClientId] [int] NOT NULL,
+ CONSTRAINT [PK_ClientProperties] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+ -- Create Client Redirect URIs
+CREATE TABLE [dbo].[ClientRedirectUris](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[RedirectUri] [nvarchar](2000) NOT NULL,
+	[ClientId] [int] NOT NULL,
+ CONSTRAINT [PK_ClientRedirectUris] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+ -- Create Clients
+ CREATE TABLE [dbo].[Clients](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[Enabled] [bit] NOT NULL,
+	[ClientId] [nvarchar](200) NOT NULL,
+	[ProtocolType] [nvarchar](200) NOT NULL,
+	[RequireClientSecret] [bit] NOT NULL,
+	[ClientName] [nvarchar](200) NULL,
+	[Description] [nvarchar](1000) NULL,
+	[ClientUri] [nvarchar](2000) NULL,
+	[LogoUri] [nvarchar](2000) NULL,
+	[RequireConsent] [bit] NOT NULL,
+	[AllowRememberConsent] [bit] NOT NULL,
+	[AlwaysIncludeUserClaimsInIdToken] [bit] NOT NULL,
+	[RequirePkce] [bit] NOT NULL,
+	[AllowPlainTextPkce] [bit] NOT NULL,
+	[AllowAccessTokensViaBrowser] [bit] NOT NULL,
+	[FrontChannelLogoutUri] [nvarchar](2000) NULL,
+	[FrontChannelLogoutSessionRequired] [bit] NOT NULL,
+	[BackChannelLogoutUri] [nvarchar](2000) NULL,
+	[BackChannelLogoutSessionRequired] [bit] NOT NULL,
+	[AllowOfflineAccess] [bit] NOT NULL,
+	[IdentityTokenLifetime] [int] NOT NULL,
+	[AccessTokenLifetime] [int] NOT NULL,
+	[AuthorizationCodeLifetime] [int] NOT NULL,
+	[ConsentLifetime] [int] NULL,
+	[AbsoluteRefreshTokenLifetime] [int] NOT NULL,
+	[SlidingRefreshTokenLifetime] [int] NOT NULL,
+	[RefreshTokenUsage] [int] NOT NULL,
+	[UpdateAccessTokenClaimsOnRefresh] [bit] NOT NULL,
+	[RefreshTokenExpiration] [int] NOT NULL,
+	[AccessTokenType] [int] NOT NULL,
+	[EnableLocalLogin] [bit] NOT NULL,
+	[IncludeJwtId] [bit] NOT NULL,
+	[AlwaysSendClientClaims] [bit] NOT NULL,
+	[ClientClaimsPrefix] [nvarchar](200) NULL,
+	[PairWiseSubjectSalt] [nvarchar](200) NULL,
+	[Created] [datetime2](7) NOT NULL,
+	[Updated] [datetime2](7) NULL,
+	[LastAccessed] [datetime2](7) NULL,
+	[UserSsoLifetime] [int] NULL,
+	[UserCodeType] [nvarchar](100) NULL,
+	[DeviceCodeLifetime] [int] NOT NULL,
+	[NonEditable] [bit] NOT NULL,
+ CONSTRAINT [PK_Clients] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+ -- Create Client Scopes 
+ CREATE TABLE [dbo].[ClientScopes](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[Scope] [nvarchar](200) NOT NULL,
+	[ClientId] [int] NOT NULL,
+ CONSTRAINT [PK_ClientScopes] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+ -- Create Client Secrets
+CREATE TABLE [dbo].[ClientSecrets](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[Description] [nvarchar](2000) NULL,
+	[Value] [nvarchar](4000) NOT NULL,
+	[Expiration] [datetime2](7) NULL,
+	[Type] [nvarchar](250) NOT NULL,
+	[Created] [datetime2](7) NOT NULL,
+	[ClientId] [int] NOT NULL,
+ CONSTRAINT [PK_ClientSecrets] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+ -- Create Identity Claims
+CREATE TABLE [dbo].[IdentityClaims](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[Type] [nvarchar](200) NOT NULL,
+	[IdentityResourceId] [int] NOT NULL,
+ CONSTRAINT [PK_IdentityClaims] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+ -- Create Identity Properties
+CREATE TABLE [dbo].[IdentityProperties](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[Key] [nvarchar](250) NOT NULL,
+	[Value] [nvarchar](2000) NOT NULL,
+	[IdentityResourceId] [int] NOT NULL,
+ CONSTRAINT [PK_IdentityProperties] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+ -- Create Identity Resources
+ CREATE TABLE [dbo].[IdentityResources](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[Enabled] [bit] NOT NULL,
+	[Name] [nvarchar](200) NOT NULL,
+	[DisplayName] [nvarchar](200) NULL,
+	[Description] [nvarchar](1000) NULL,
+	[Required] [bit] NOT NULL,
+	[Emphasize] [bit] NOT NULL,
+	[ShowInDiscoveryDocument] [bit] NOT NULL,
+	[Created] [datetime2](7) NOT NULL,
+	[Updated] [datetime2](7) NULL,
+	[NonEditable] [bit] NOT NULL,
+ CONSTRAINT [PK_IdentityResources] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+
+
+
+
+
+-- RELATIONSHIP AREA
+
+
+
+
+
+ --Create API Claims - Relationship
+ALTER TABLE [dbo].[ApiClaims]  WITH CHECK ADD  CONSTRAINT [FK_ApiClaims_ApiResources_ApiResourceId] FOREIGN KEY([ApiResourceId])
+REFERENCES [dbo].[ApiResources] ([Id])
+ON DELETE CASCADE
+GO
+
+ALTER TABLE [dbo].[ApiClaims] CHECK CONSTRAINT [FK_ApiClaims_ApiResources_ApiResourceId]
+GO
+ -- Create API Properties - Relationship
+ ALTER TABLE [dbo].[ApiProperties]  WITH CHECK ADD  CONSTRAINT [FK_ApiProperties_ApiResources_ApiResourceId] FOREIGN KEY([ApiResourceId])
+REFERENCES [dbo].[ApiResources] ([Id])
+ON DELETE CASCADE
+GO
+
+ALTER TABLE [dbo].[ApiProperties] CHECK CONSTRAINT [FK_ApiProperties_ApiResources_ApiResourceId]
+GO
+
+ -- Create API Resources - Relationship
+ 
+
+ -- Create API Scope Claims - Relationship
+ ALTER TABLE [dbo].[ApiScopeClaims]  WITH CHECK ADD  CONSTRAINT [FK_ApiScopeClaims_ApiScopes_ApiScopeId] FOREIGN KEY([ApiScopeId])
+REFERENCES [dbo].[ApiScopes] ([Id])
+ON DELETE CASCADE
+GO
+
+ALTER TABLE [dbo].[ApiScopeClaims] CHECK CONSTRAINT [FK_ApiScopeClaims_ApiScopes_ApiScopeId]
+GO
+ -- Create API Scope - Relationship
+ ALTER TABLE [dbo].[ApiScopes]  WITH CHECK ADD  CONSTRAINT [FK_ApiScopes_ApiResources_ApiResourceId] FOREIGN KEY([ApiResourceId])
+REFERENCES [dbo].[ApiResources] ([Id])
+ON DELETE CASCADE
+GO
+
+ALTER TABLE [dbo].[ApiScopes] CHECK CONSTRAINT [FK_ApiScopes_ApiResources_ApiResourceId]
+GO
+ -- Create API Secret - Relationship
+ ALTER TABLE [dbo].[ApiSecrets]  WITH CHECK ADD  CONSTRAINT [FK_ApiSecrets_ApiResources_ApiResourceId] FOREIGN KEY([ApiResourceId])
+REFERENCES [dbo].[ApiResources] ([Id])
+ON DELETE CASCADE
+GO
+
+ALTER TABLE [dbo].[ApiSecrets] CHECK CONSTRAINT [FK_ApiSecrets_ApiResources_ApiResourceId]
+GO
+ -- Create Client Claims - Relationship
+  ALTER TABLE [dbo].[ClientClaims]  WITH CHECK ADD  CONSTRAINT [FK_ClientClaims_Clients_ClientId] FOREIGN KEY([ClientId])
+REFERENCES [dbo].[Clients] ([Id])
+ON DELETE CASCADE
+GO
+
+ALTER TABLE [dbo].[ClientClaims] CHECK CONSTRAINT [FK_ClientClaims_Clients_ClientId]
+GO
+ -- Create Client Cors Origins - Relationship
+  ALTER TABLE [dbo].[ClientCorsOrigins]  WITH CHECK ADD  CONSTRAINT [FK_ClientCorsOrigins_Clients_ClientId] FOREIGN KEY([ClientId])
+REFERENCES [dbo].[Clients] ([Id])
+ON DELETE CASCADE
+GO
+
+ALTER TABLE [dbo].[ClientCorsOrigins] CHECK CONSTRAINT [FK_ClientCorsOrigins_Clients_ClientId]
+GO
+ -- Create Client Grant Type - Relationship
+  ALTER TABLE [dbo].[ClientGrantTypes]  WITH CHECK ADD  CONSTRAINT [FK_ClientGrantTypes_Clients_ClientId] FOREIGN KEY([ClientId])
+REFERENCES [dbo].[Clients] ([Id])
+ON DELETE CASCADE
+GO
+
+ALTER TABLE [dbo].[ClientGrantTypes] CHECK CONSTRAINT [FK_ClientGrantTypes_Clients_ClientId]
+GO
+ -- Create Client IDP Restrictions - Relationship
+ ALTER TABLE [dbo].[ClientIdPRestrictions]  WITH CHECK ADD  CONSTRAINT [FK_ClientIdPRestrictions_Clients_ClientId] FOREIGN KEY([ClientId])
+REFERENCES [dbo].[Clients] ([Id])
+ON DELETE CASCADE
+GO
+
+ALTER TABLE [dbo].[ClientIdPRestrictions] CHECK CONSTRAINT [FK_ClientIdPRestrictions_Clients_ClientId]
+GO
+ -- Create Client Post Logout Redirect UriS - Relationship
+ ALTER TABLE [dbo].[ClientPostLogoutRedirectUris]  WITH CHECK ADD  CONSTRAINT [FK_ClientPostLogoutRedirectUris_Clients_ClientId] FOREIGN KEY([ClientId])
+REFERENCES [dbo].[Clients] ([Id])
+ON DELETE CASCADE
+GO
+
+ALTER TABLE [dbo].[ClientPostLogoutRedirectUris] CHECK CONSTRAINT [FK_ClientPostLogoutRedirectUris_Clients_ClientId]
+GO
+ -- Create Client Properties - Relationship
+ALTER TABLE [dbo].[ClientProperties]  WITH CHECK ADD  CONSTRAINT [FK_ClientProperties_Clients_ClientId] FOREIGN KEY([ClientId])
+REFERENCES [dbo].[Clients] ([Id])
+ON DELETE CASCADE
+GO
+
+ALTER TABLE [dbo].[ClientProperties] CHECK CONSTRAINT [FK_ClientProperties_Clients_ClientId]
+GO
+ -- Create Client Redirect URIs - Relationship
+ ALTER TABLE [dbo].[ClientRedirectUris]  WITH CHECK ADD  CONSTRAINT [FK_ClientRedirectUris_Clients_ClientId] FOREIGN KEY([ClientId])
+REFERENCES [dbo].[Clients] ([Id])
+ON DELETE CASCADE
+GO
+
+ALTER TABLE [dbo].[ClientRedirectUris] CHECK CONSTRAINT [FK_ClientRedirectUris_Clients_ClientId]
+GO
+ -- Create Clients - Relationship
+
+ -- Create Client Scopes - Relationship 
+ ALTER TABLE [dbo].[ClientScopes]  WITH CHECK ADD  CONSTRAINT [FK_ClientScopes_Clients_ClientId] FOREIGN KEY([ClientId])
+REFERENCES [dbo].[Clients] ([Id])
+ON DELETE CASCADE
+GO
+
+ALTER TABLE [dbo].[ClientScopes] CHECK CONSTRAINT [FK_ClientScopes_Clients_ClientId]
+GO
+
+ -- Create Client Secrets - Relationship
+ALTER TABLE [dbo].[ClientSecrets]  WITH CHECK ADD  CONSTRAINT [FK_ClientSecrets_Clients_ClientId] FOREIGN KEY([ClientId])
+REFERENCES [dbo].[Clients] ([Id])
+ON DELETE CASCADE
+GO
+
+ALTER TABLE [dbo].[ClientSecrets] CHECK CONSTRAINT [FK_ClientSecrets_Clients_ClientId]
+GO
+
+ -- Create Identity Claims - Relationship
+ALTER TABLE [dbo].[IdentityClaims]  WITH CHECK ADD  CONSTRAINT [FK_IdentityClaims_IdentityResources_IdentityResourceId] FOREIGN KEY([IdentityResourceId])
+REFERENCES [dbo].[IdentityResources] ([Id])
+ON DELETE CASCADE
+GO
+
+ALTER TABLE [dbo].[IdentityClaims] CHECK CONSTRAINT [FK_IdentityClaims_IdentityResources_IdentityResourceId]
+GO
+ -- Create Identity Properties - Relationship
+ALTER TABLE [dbo].[IdentityProperties]  WITH CHECK ADD  CONSTRAINT [FK_IdentityProperties_IdentityResources_IdentityResourceId] FOREIGN KEY([IdentityResourceId])
+REFERENCES [dbo].[IdentityResources] ([Id])
+ON DELETE CASCADE
+GO
+
+ALTER TABLE [dbo].[IdentityProperties] CHECK CONSTRAINT [FK_IdentityProperties_IdentityResources_IdentityResourceId]
+GO
+ -- Create Identity Resources - Relationship
+
+
+
+
+ -- OPERATION AREA
+
+ CREATE TABLE [dbo].[DeviceCodes](
+	[DeviceCode] [nvarchar](200) NOT NULL,
+	[UserCode] [nvarchar](200) NOT NULL,
+	[SubjectId] [nvarchar](200) NULL,
+	[ClientId] [nvarchar](200) NOT NULL,
+	[CreationTime] [datetime2](7) NOT NULL,
+	[Expiration] [datetime2](7) NOT NULL,
+	[Data] [nvarchar](max) NOT NULL,
+ CONSTRAINT [PK_DeviceCodes] PRIMARY KEY CLUSTERED 
+(
+	[UserCode] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
+
+CREATE TABLE [dbo].[PersistedGrants](
+	[Key] [nvarchar](200) NOT NULL,
+	[Type] [nvarchar](50) NOT NULL,
+	[SubjectId] [nvarchar](200) NULL,
+	[ClientId] [nvarchar](200) NOT NULL,
+	[CreationTime] [datetime2](7) NOT NULL,
+	[Expiration] [datetime2](7) NULL,
+	[Data] [nvarchar](max) NOT NULL,
+ CONSTRAINT [PK_PersistedGrants] PRIMARY KEY CLUSTERED 
+(
+	[Key] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
